55f277d6fbcb83d9826a32f07e7c23161e81c6be
Harshit1Rathor <93603096+Harshit1Rathor@users.noreply.github.com>
2023-10-02 12:46:13 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,8 @@
+header('Access-Control-Allow-Origin:  *');  
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');   
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+
 
 const Product = require("../models/product");
 const getAllProducts = async (req, res)=>{
