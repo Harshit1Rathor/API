1dbf67ab5b7d48d144e9ec2974d73efcccc57f1c
Harshit1Rathor <93603096+Harshit1Rathor@users.noreply.github.com>
2023-10-02 12:41:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+header('Access-Control-Allow-Origin:  *');  
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');   
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+
 require("dotenv").config();
 const express = require("express");
 const app = express();
